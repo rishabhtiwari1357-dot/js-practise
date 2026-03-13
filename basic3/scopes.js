60fd6e458e01c064d9e5fc83e99b4d6e1bd730eb
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-13 12:41:27 +0000

    expl_scopes

--- basic3/scopes.js
+++ basic3/scopes.js
@@ -4,9 +4,52 @@ if (true) {
   let a = 10;
   const b = 20;
   var c = 30;
-  console.log(`INNER ${a}`)
+  // console.log(`INNER ${a}`)
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+  const username = "rishabh"
+  
+  function two(){
+    const website = "youtube"
+    console.log(username)
+  }
+  // console.log(website)
+
+  two()
+
+}
+
+// one()
+
+if (true){
+  const username = "rishabh"
+  if (username === "rishabh"){
+    const website = " youtube"
+    // console.log(username + website)
+  }
+  // console.log(website);
+}
+// console.log(username)
+
+
+// ++++++++++++++++++interesting++++++++++++++++++++
+
+function addOne(num){
+  return num + 1;
+}
+
+addOne(6)
+
+const addTwo = function(num){
+  return num +2
+}
+
+addTwo()
+
+
