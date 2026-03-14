effc805fd943561b7a5f8f2e14922be145a52c28
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-14 03:28:50 +0000

    adv_control_flow

--- control_flow/one.js
+++ control_flow/one.js
@@ -1,5 +1,11 @@
 // if
 
-if (2 === "2"){
-    console.log("executed")
+const isLoggedIN = true
+const temperature = 41
+
+
+if ( temperature < 50){
+    console.log("cannot boil water")
+} else{
+    console.log("water can be boiled")
 }
