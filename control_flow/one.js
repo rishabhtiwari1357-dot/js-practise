0324136921a7d35845e6e198960cb791574b1197
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-14 04:27:33 +0000

    exp_conditinals

--- control_flow/one.js
+++ control_flow/one.js
@@ -4,8 +4,33 @@ const isLoggedIN = true
 const temperature = 41
 
 
-if ( temperature < 50){
-    console.log("cannot boil water")
-} else{
-    console.log("water can be boiled")
+// if ( temperature < 50){
+//     console.log("cannot boil water")
+// } else{
+//     console.log("water can be boiled")
+// }
+
+
+// const score = 200
+
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+
+const balance = 1000
+
+// if (balance > 500) console.log("enough balance");
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard){
+    console.log(" You are allowed to buy courses")
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
 }
