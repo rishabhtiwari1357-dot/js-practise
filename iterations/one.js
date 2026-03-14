452699738f1ed4dcf22341c78f4c9a7d6bc6f4c2
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-14 10:16:55 +0000

    exp_loops

---
+++ iterations/one.js
@@ -0,0 +1,23 @@
+for (let index = 0; index < 10; index++) {
+    // const element = array[index];
+    console.log(index)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
