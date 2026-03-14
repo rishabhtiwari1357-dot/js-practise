6ecb776567be461afc04e2bf42e9d58f35403f20
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-14 07:01:32 +0000

    exp_truthy

---
+++ control_flow/truthy.js
@@ -0,0 +1,11 @@
+const userEmail = []
+
+if(userEmail){
+    console.log("Got user email")
+} else {
+    console.log("don't have user email")
+}
+
+if (userEmail.length === 0){
+    console.log("array is empty")
+}
