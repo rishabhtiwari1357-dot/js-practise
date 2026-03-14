2db4bae20711327d8c17b599aa1bf953f109245f
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-14 15:23:51 +0000

    exp_ conditionals

--- iterations/one.js
+++ iterations/one.js
@@ -1,8 +1,18 @@
-for (let index = 0; index < 10; index++) {
-    // const element = array[index];
-    console.log(index)
+for (let i = 0; i <= 10; i++) {
+    // const element = array[i];
+    // console.log(i)
 }
 
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop value: ${i}`)
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and outer loop ${i}`)
+        console.log(i + '*' + j + '=' + i*j);
+        
+    }
+    
+}
 
 
 
