46e7fa0c1235cbd4908ba05993e9291636cc1fd8
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-15 07:06:40 +0000

    exp_loops

--- iterations/one.js
+++ iterations/one.js
@@ -29,13 +29,15 @@ for (let i = 0; i <= 10; i++) {
 
 for (let index = 1; index <= 20; index++) {
 
-    if(index ==5){
+    if(index == 5){
         console.log(`detected 5`)
         continue;
     }
     console.log(`i is ${index}`)
     
 }
+
+
 
 
 
