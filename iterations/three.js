d180cd20f2152f1ce3e8e5908be5166c3e6ef6d4
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-16 13:49:30 +0000

    exploring

--- iterations/three.js
+++ iterations/three.js
@@ -2,6 +2,16 @@
 
 const arr = [1, 2, 3, 4, 5]
 for (const element of arr) {
-    console.log(element);
+    // console.log(element);
     
+}
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('USA', "United States of America")
+
+// console.log(map)
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value)
 }
