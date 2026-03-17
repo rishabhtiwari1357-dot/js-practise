df79d97d05707c7688111e59efa8d6af9c06cc7c
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-17 06:27:47 +0000

    done_with_for_in

--- iterations/four.js
+++ iterations/four.js
@@ -5,6 +5,12 @@ const myObj = {
     swift: 'swift by app'
 }
 
-for (const element in myObj) {
-   console.log(`${element} is shortcut for ${myObj[element]}`)
+// for (const element in myObj) {
+//    console.log(`${element} is shortcut for ${myObj[element]}`)
+// }
+
+const progg_lang = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in progg_lang) {
+    console.log(progg_lang[key]);
 }
